8b9c2817226180060d164df6b0930ba72dde26f0
nolachen <nolachen@gmail.com>
2017-04-29 20:41:51 -0400

    stuff

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var path = require('path')
 var express = require('express')
 var app = express()
-var port = process.env.PORT || 5000
-app.get('/:html_file', function(req, res) {
-  res.sendFile(path.join(__dirname, 'views', req.params.html_file + '.html'))
+var port = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.use('/assets/', express.static(__dirname + '/assets'));
+
+app.get('/', function(req, res) {
+  //res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index');
+
 })
 app.listen(port, function() {
   console.log('Listening on port: ' + port)
