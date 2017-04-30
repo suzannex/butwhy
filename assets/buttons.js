1db5965264c7384bf20729672b72d73987042437
Suzanne <becker.su@husky.neu.edu>
2017-04-29 22:04:32 -0400

    added use of google api

--- assets/buttons.js
+++ assets/buttons.js
@@ -1,7 +1,34 @@
+var george = new google.maps.LatLng(42.3509178,-71.111145);
+
 $(document).ready(function() {
     $('#inspiration-form').on('submit', function(e) {
         e.preventDefault();
         var inputText = $('#inspiration-input').val();
         console.log(inputText);
+        var result = doSearch('coffee');
+        $(#textfield).val(result);
     })
 });
+
+function doSearch(searchText) {
+	service = new google.maps.places.PlacesService(map);
+	service.textSearch(createRequest(searchText), callback);
+}
+
+function createRequest(searchText) {
+	return {
+		query: searchText,
+		location: new google.maps.LatLng(42.3509178,-71.111145),
+		//location: george,
+		radius: 1000
+	};
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+  	var toLog = results.length >= 0 ? results[0] : results.length;
+    console.log(toLog);
+    alert(toLog);
+    return toLog;
+  }
+}
