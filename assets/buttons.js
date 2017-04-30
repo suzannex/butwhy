c81495efe20f2328b94f8307df983b4adab5083c
Suzanne <becker.su@husky.neu.edu>
2017-04-29 23:48:53 -0400

    improvements to google api use

--- assets/buttons.js
+++ assets/buttons.js
@@ -1,22 +1,42 @@
-function initMap() {
-	service = new google.maps.places.PlacesService(map);
-	service.textSearch(createRequest('coffee'), callback);
-}
+var textInput, mapsSearchResult, service, map;
 
-var george = new google.maps.LatLng(42.3509178,-71.111145);
+window.initMap = function() {
+	console.log('initMap ready');
+	console.log(google);
+
+	var george = new google.maps.LatLng(42.3509178,-71.111145); // current location
+
+	map = new google.maps.Map(document.getElementById('map'), {
+    	center: george,
+      	zoom: 15
+    });
+
+	service = new google.maps.places.PlacesService(map);
+	//service.textSearch(createRequest('coffee'), callback);
+};
 
 $(document).ready(function() {
+	textInput = $('#inspiration-input');
+	console.log('doc ready');
     $('#inspiration-form').on('submit', function(e) {
+    	// prevent page reload
         e.preventDefault();
-        var inputText = $('#inspiration-input').val();
+
+        var inputText = textInput.val();
         console.log(inputText);
-        var result = doSearch('coffee');
-        $(#textfield).val(result);
+        updateButtons(inputText);
+
+        // clear the text field
+        textInput.val("");
     })
 });
 
-function doSearch(searchText) {
-	service = new google.maps.places.PlacesService(map);
+function updateButtons(searchText) {
+    googleMapsSearch(searchText);
+}
+
+function googleMapsSearch(searchText) {
+	//service = new google.maps.places.PlacesService(map);
 	service.textSearch(createRequest(searchText), callback);
 }
 
@@ -25,15 +45,21 @@ function createRequest(searchText) {
 		query: searchText,
 		location: new google.maps.LatLng(42.3509178,-71.111145),
 		//location: george,
-		radius: 1000
+		radius: 100
 	};
 }
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
+  	if(results.length >= 0) {
+  		mapSearchResult = results[0];
+  	}
   	var toLog = results.length >= 0 ? results[0] : results.length;
     console.log(toLog);
-    alert(toLog);
-    return toLog;
   }
 }
+
+
+
+
+
